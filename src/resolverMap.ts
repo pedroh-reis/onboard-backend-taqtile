948c72b040419e228db71a52e702e559da565440
Pedro Henrique Carvalho dos Reis <phenrique.carvalho.reis@gmail.com>
2021-09-12 23:41:34 +0200

    integrating with the db

--- src/resolverMap.ts
+++ src/resolverMap.ts
@@ -1,4 +1,6 @@
 import {IResolvers} from "@graphql-tools/utils"
+import {getRepository} from "typeorm"
+import {User} from "./entity/User"
 const resolverMap: IResolvers = {
     Query: {
         hello(_: void, args: void): string {
@@ -7,12 +9,36 @@ const resolverMap: IResolvers = {
     },
 
     Mutation: {
-        createUser(_, args) {
+        async createUser(_, args) {
+            const userRepository = getRepository(User)
+
+            let dbUser = await userRepository.findOne({email: args.input.email})
+            if (dbUser) {
+                throw new Error("ERROR: User already created.")
+            }
+
+            const user = new User()
+            user.name = args.input.name
+            user.email = args.input.email
+            user.password = args.input.password
+            user.birthDate = args.input.birthDate
+
+            if (user.password.length < 7) {
+                throw new Error("ERROR: Password should have at least 7 characters.")
+            }
+
+            const passwordRegex = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i
+            if (!passwordRegex.test(user.password)) {
+                throw new Error("ERROR: Weak password.")
+            }
+
+            dbUser = await userRepository.save(user)
+
             return {
-                id: 123,
-                name: args.input.name,
-                email: args.input.email,
-                birthDate: args.input.birthDate
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                birthDate: dbUser.birthDate
             }
         }
     }
